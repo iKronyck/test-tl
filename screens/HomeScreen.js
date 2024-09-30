811fe31c9a82d9bd4b02142f0895a2d443c872fd
Rene Alas <rene.alaslopez@telus.com>
2024-09-30 11:42:08 -0600

    Added the missing imports from HomePage for challenge

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
 
 export default function HomeScreen({ navigation }) {
-
   return (
     <View style={styles.container}>
       {/* Here add your Code for Task List and button to navigate to add new tasks, also remember todo's should be able to be deleted. */}
@@ -13,5 +13,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16
-  },
+  }
 })
